8cc88d7fe51a68d18417a9146838cd55d1818400
Diana-Batenina <dbatenina@mail.ru>
2020-12-09 17:45:08 +0500

    Задание 6 выполнено

---
+++ task06.1/task06.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task06._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите, пожалуйста, целое число");
+            int n = int.Parse(Console.ReadLine());
+            var remain2 = n % 2;
+            var remain3 = n % 3;
+            bool a = (remain2 == 0) || (remain3 == 0);
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task06.2/task06.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task06._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите, пожалуйста, координаты точки");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("x = " + x);
+            double y = double.Parse(Console.ReadLine());
+            Console.WriteLine("y = " + y);
+
+            bool checkX = (x >= -1) && (x <= 1.5);
+            bool checkY = (y >= -0.5) && (y <= 2);
+            if ((checkX == true) & (checkY == true))
+                Console.WriteLine("Точка принадлежит указанной области");
+            else
+                Console.WriteLine("Точка не принадлежит указанной области");
+
+            Console.ReadKey();
+        }
+    }
+}

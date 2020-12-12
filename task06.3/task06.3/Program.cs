e11df236777ad7c9a51c0c6843c10e402ba2100a
Diana-Batenina <dbatenina@mail.ru>
2020-12-12 16:55:22 +0500

    Задача 3 задания 6 выполнена

---
+++ task06.3/task06.3/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task06._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите, пожалуйста, позицию белого короля");
+            var whiteKingPosition = Console.ReadLine();
+            Console.WriteLine("Введите, пожалуйста, позицию чёрной ладьи");
+            var blackRookPosition = Console.ReadLine();
+
+            if (CheckPosition(whiteKingPosition, blackRookPosition))
+            {
+                Console.WriteLine("Введите ход белого короля");
+                var whiteKingMove = Console.ReadLine();
+                if (CanKingMakeSafeMove(whiteKingMove, whiteKingMove, blackRookPosition))
+                    Console.WriteLine("Король может ходить");
+                else
+                    Console.WriteLine("Король не может ходить");
+            }
+            else
+                Console.WriteLine("Начальная расстановка фигур некорректна");
+
+            Console.ReadKey();
+        }
+
+        static void GetCoordinates(string position, out int x, out int y)
+        {
+            x = (int)position[0] - 0x60;
+            y = int.Parse(position[1].ToString());
+        }
+
+        //King and Rook
+
+        static bool CanKingMakeMove(string start, string end)
+        {
+            if (start == end)
+                return false;
+            
+            int startX, startY, endX, endY;
+
+            GetCoordinates(start, out startX, out startY);
+            GetCoordinates(end, out endX, out endY);
+
+            return Math.Abs(endX - startX) <= 1 && Math.Abs(endY - startY) <= 1;
+        }
+
+        static bool CanRookMakeMove(string start, string end)
+        {
+            if (start == end)
+                return false;
+
+            int startX, startY, endX, endY;
+
+            GetCoordinates(start, out startX, out startY);
+            GetCoordinates(end, out endX, out endY);
+
+            return startX == endX || startY == endY;
+        }
+
+        static bool CheckPosition (string whitePosition, string blackPosition)
+        {
+            return whitePosition != blackPosition && !CanKingMakeMove(whitePosition, blackPosition) && !CanRookMakeMove(blackPosition, whitePosition);
+        }
+
+        static bool CanKingMakeSafeMove (string kingStartPosition, string kingNewPosition, string rookPosition)
+        {
+            return CanKingMakeMove(kingStartPosition, kingNewPosition) && !CanRookMakeMove(rookPosition, kingNewPosition);
+        }
+    }
+}

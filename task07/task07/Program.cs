6d78de3415a8f06f86968563a7d966edca6f581f
Diana-Batenina <dbatenina@mail.ru>
2020-12-23 22:26:02 +0500

    Задание 7 выполнено Судя по второй строчке, условие равносильно "х == 0", и тогда можно либо убрать его, оставив "х<=0", либо изменить первое условие. Хотя при х==0 и f(x)=0, поскольку он равен х, так что, наверное, можно было оставить первое условие неизменным и убрать второе, если там не опечатка

---
+++ task07/task07/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите, пожалуйста, значение x");
+            var x = double.Parse(Console.ReadLine());
+            double f;
+
+            if (x < 0)
+            {
+                f = 0;
+                Console.WriteLine($"f(x) = {f}");
+            }
+            else
+                if (x == 0)
+                {
+                    f = x;
+                    Console.WriteLine($"f(x) = {f}");
+                }
+            else
+            {
+                f = x * x;
+                Console.WriteLine($"f(x) = {f}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
